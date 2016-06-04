557cdcdbb25712cede98d7fb330d9514b92373c3
Winston Waters <winstonwaters@Macintosh.local>
2016-06-04 15:13:04 -0400

    initialize upload. no style or big image

---
+++ data.js
@@ -0,0 +1,199 @@
+var albums = [
+  { id: "Barcelona",
+    title: "24 Hours in Barcelona",
+    cover: "travel_blog_images/Barcelona/Barcelona-1.jpg",
+    pictures: [
+      {
+      caption: "Beaches, mansions, and great views: a peak inside Morgan's beautiful hometown of Barcelona.",
+      photo: "travel_blog_images/Barcelona/Barcelona-1.jpg",
+      },
+      {
+      caption: "Beaches, mansions, and great views: a peak inside Morgan's beautiful hometown of Barcelona.",
+      photo: "travel_blog_images/Barcelona/Barcelona-2.jpg",
+      },
+      {
+      caption: "Beaches, mansions, and great views: a peak inside Morgan's beautiful hometown of Barcelona.",
+      photo: "travel_blog_images/Barcelona/Barcelona-3.jpg",
+      },
+      {
+      caption: "Beaches, mansions, and great views: a peak inside Morgan's beautiful hometown of Barcelona.",
+      photo: "travel_blog_images/Barcelona/Barcelona-4.jpg",
+      },
+      {
+      caption: "Beaches, mansions, and great views: a peak inside Morgan's beautiful hometown of Barcelona.",
+      photo: "travel_blog_images/Barcelona/Barcelona-5.jpg",
+      },
+      {
+      caption: "Beaches, mansions, and great views: a peak inside Morgan's beautiful hometown of Barcelona.",
+      photo: "travel_blog_images/Barcelona/Barcelona-6.jpg",
+    }
+  ]
+  },
+  { id: "Copenhagen",
+    title: "24 Hours in Copenhagen",
+    cover: "travel_blog_images/Copenhagen/Copenhagen-1.jpg",
+    pictures: [
+      {
+      caption: "Fashion, food, and the happiest people on earth. Welcome to Copenhagen!",
+      photo: "travel_blog_images/Copenhagen/Copenhagen-1.jpg",
+      },
+      {
+      caption: "Fashion, food, and the happiest people on earth. Welcome to Copenhagen!",
+      photo: "travel_blog_images/Copenhagen/Copenhagen-2.jpg",
+      },
+      {
+      caption: "Fashion, food, and the happiest people on earth. Welcome to Copenhagen!",
+      photo: "travel_blog_images/Copenhagen/Copenhagen-3.jpg",
+      },
+      {
+      caption: "Fashion, food, and the happiest people on earth. Welcome to Copenhagen!",
+      photo: "travel_blog_images/Copenhagen/Copenhagen-4.jpg",
+      },
+      {
+      caption: "Fashion, food, and the happiest people on earth. Welcome to Copenhagen!",
+      photo: "travel_blog_images/Copenhagen/Copenhagen-5.jpg",
+      },
+      {
+      caption: "Fashion, food, and the happiest people on earth. Welcome to Copenhagen!",
+      photo: "travel_blog_images/Copenhagen/Copenhagen-6.jpg",
+      },
+      {
+      caption: "Fashion, food, and the happiest people on earth. Welcome to Copenhagen!",
+      photo: "travel_blog_images/Copenhagen/Copenhagen-7.jpg",
+      }
+    ]
+  },
+  { id: "Helsinksi",
+    title: "24 Hours in Helsinksi",
+    cover: "travel_blog_images/Helsinksi/Helsinksi-1.jpg",
+    pictures: [
+      {
+      caption: "Get inspired and experience the best of Scandinavia in the Finnish capital.",
+      photo: "travel_blog_images/Helsinksi/Helsinksi-1.jpg",
+      },
+      {
+      caption: "Get inspired and experience the best of Scandinavia in the Finnish capital.",
+      photo: "travel_blog_images/Helsinksi/Helsinksi-2.jpg",
+      },
+      {
+      caption: "Get inspired and experience the best of Scandinavia in the Finnish capital.",
+      photo: "travel_blog_images/Helsinksi/Helsinksi-3.jpg",
+      },
+      {
+      caption: "Get inspired and experience the best of Scandinavia in the Finnish capital.",
+      photo: "travel_blog_images/Helsinksi/Helsinksi-4.jpg",
+      },
+      {
+      caption: "Get inspired and experience the best of Scandinavia in the Finnish capital.",
+      photo: "travel_blog_images/Helsinksi/Helsinksi-5.jpg",
+      },
+      {
+      caption: "Get inspired and experience the best of Scandinavia in the Finnish capital.",
+      photo: "travel_blog_images/Helsinksi/Helsinksi-6.jpg",
+      },
+      {
+      caption: "Get inspired and experience the best of Scandinavia in the Finnish capital.",
+      photo: "travel_blog_images/Helsinksi/Helsinksi-7.jpg",
+      },
+      {
+      caption: "Get inspired and experience the best of Scandinavia in the Finnish capital.",
+      photo: "travel_blog_images/Helsinksi/Helsinksi-8.jpg",
+      }
+    ]
+  },
+  { id: "London",
+    title: "24 Hours in London",
+    cover: "travel_blog_images/London/London-1.jpg",
+    pictures: [
+      {
+      caption: "So much to see, so little time: Let's waste no time and spend 12hrs in London!",
+      photo: "travel_blog_images/London/London-1.jpg",
+      },
+      {
+      caption: "So much to see, so little time: Let's waste no time and spend 12hrs in London!",
+      photo: "travel_blog_images/London/London-2.jpg",
+      },
+      {
+      caption: "So much to see, so little time: Let's waste no time and spend 12hrs in London!",
+      photo: "travel_blog_images/London/London-3.jpg",
+      },
+      {
+      caption: "So much to see, so little time: Let's waste no time and spend 12hrs in London!",
+      photo: "travel_blog_images/London/London-4.jpg",
+      },
+      {
+      caption: "So much to see, so little time: Let's waste no time and spend 12hrs in London!",
+      photo: "travel_blog_images/London/London-5.jpg",
+      },
+      {
+      caption: "So much to see, so little time: Let's waste no time and spend 12hrs in London!",
+      photo: "travel_blog_images/London/London-6.jpg",
+      },
+      {
+      caption: "So much to see, so little time: Let's waste no time and spend 12hrs in London!",
+      photo: "travel_blog_images/London/London-7.jpg",
+      }
+    ]
+  },
+  { id: "Marseille",
+    title: "24 Hours in Marseille",
+    cover: "travel_blog_images/Marseille/Marseille-1.jpg",
+    caption: "Soak in some sun, discover the city and experience world-famous architecture in Marseille.",
+    pictures: [
+      {
+      caption: "Soak in some sun, discover the city and experience world-famous architecture in Marseille.",
+      photo: "travel_blog_images/Marseille/Marseille-1.jpg",
+      },
+      {
+      caption: "Soak in some sun, discover the city and experience world-famous architecture in Marseille.",
+      photo: "travel_blog_images/Marseille/Marseille-2.jpg",
+      },
+      {
+      caption: "Soak in some sun, discover the city and experience world-famous architecture in Marseille.",
+      photo: "travel_blog_images/Marseille/Marseille-3.jpg",
+      },
+      {
+      caption: "Soak in some sun, discover the city and experience world-famous architecture in Marseille.",
+      photo: "travel_blog_images/Marseille/Marseille-4.jpg",
+      },
+      {
+      caption: "Soak in some sun, discover the city and experience world-famous architecture in Marseille.",
+      photo: "travel_blog_images/Marseille/Marseille-5.jpg",
+      },
+      {
+      caption: "Soak in some sun, discover the city and experience world-famous architecture in Marseille.",
+      photo: "travel_blog_images/Marseille/Marseille-6.jpg",
+      }
+    ]
+  },
+  { id: "Paris",
+    title: "24 Hours in Paris",
+    cover: "travel_blog_images/Paris/Paris-1.jpg",
+    pictures: [
+      {
+      caption: "All the cliches about the city of lights? They are true! Let's fall in love with Paris.",
+      photo: "travel_blog_images/Paris/Paris-1.jpg",
+      },
+      {
+      caption: "All the cliches about the city of lights? They are true! Let's fall in love with Paris.",
+      photo: "travel_blog_images/Paris/Paris-2.jpg",
+      },
+      {
+      caption: "All the cliches about the city of lights? They are true! Let's fall in love with Paris.",
+      photo: "travel_blog_images/Paris/Paris-3.jpg",
+      },
+      {
+      caption: "All the cliches about the city of lights? They are true! Let's fall in love with Paris.",
+      photo: "travel_blog_images/Paris/Paris-4.jpg",
+      },
+      {
+      caption: "All the cliches about the city of lights? They are true! Let's fall in love with Paris.",
+      photo: "travel_blog_images/Paris/Paris-5.jpg",
+      },
+      {
+      caption: "All the cliches about the city of lights? They are true! Let's fall in love with Paris.",
+      photo: "travel_blog_images/Paris/Paris-6.jpg",
+      }
+    ]
+  },
+];
---
+++ main.js
@@ -0,0 +1,98 @@
+$(document).ready(function () {
+  event.preventDefault();
+  var albumsStr = '';
+  albums.forEach(function (element,idx,arr) {
+    albumsStr += `<div class="album" data-id="${element.id}">
+                  <h3>${element.title}</h3>
+                  <img src="${element.cover}" alt="">
+                  </div>`;
+  })
+
+$('.albums').html(albumsStr); //add albums with cover
+
+// makes the album details
+    $('.albums').on('click', '.album', function (event) {
+      event.preventDefault();
+      console.log($(this).data('id'));
+      var albumId = $(this).data('id');
+      var selectedAlbum = albums.filter(function (element, idx, arr) {
+        return element.id === albumId;
+      })
+      var photoStr = '';
+      selectedAlbum[0].pictures.forEach (function (element, idx, arr) {
+        photoStr += `<div class="photo">
+                    <img src="${element.photo}" alt"">
+                    </div>`
+      });
+      $('.albumDetail').html(photoStr);
+      $('.albumDetail').addClass('active');
+      $('.albums').hide();
+    })
+
+
+// make the photo big
+var selectingBigPhoto =
+  albums.map(function (element,idx,arr){
+  return element.id
+  })
+  selectingBigPhoto.filter(function(element){
+  return $('.photoZoom').html(selectingBigPhoto)
+})
+
+
+$('.albumDetail').on('click', function (event) {
+  event.preventDefault();
+  console.log($(this).data('pictures'));
+  var photoID = $(this).data('pictures');
+  var selectedPhoto = albums.filter(function (element, idx, arr) {
+    return element.photo === photoID;
+  })
+  var photosBig = '';
+  selectedPhoto[0].pictures.forEach(function (element, idx, arr) {
+    photosBig += `<div class="largePhoto">
+                <img src="${element.photo}" alt"">
+                <h3>${element.caption}</h3>
+                </div>`
+  });
+  $('.photoZoom').html(photosBig);
+  $('.photoZoom').addClass('active');
+  $('.albumDetail').hide();
+  $('.aside').hide();
+})
+
+// adding my sidebar
+  var listOfPlaces =
+    albums.map(function (element,idx,arr){
+    return "<li>" + element.id + "</li>"
+    })
+    listOfPlaces.forEach(function(element){
+    return $('.aside').html(listOfPlaces)
+  })
+
+//when clicking the side bar name it takes you to the photos in that album
+  $('li').on('click', function(event){
+    event.preventDefault();
+    var listID = $(this).text();
+    var selectedList = albums.filter(function (element, idx, arr) {
+      return element.id === listID;
+    })
+    var photoStr = '';
+    selectedList[0].pictures.forEach (function (element, idx, arr) {
+      photoStr += `<div class="photo">
+                  <img src="${element.photo}" alt"">
+                  </div>`
+    });
+
+// pulls album details up from navigation list
+    $('.albums').html(photoStr);
+  })
+
+
+// making header jump pages
+$('h2').on('click', function (element,idx,arr){
+  $('.albums').removeClass('hidden');
+  $('.albumDetail').removeClass('hidden');
+  $('.photoZoom').removeClass('hidden');
+  })
+
+});
